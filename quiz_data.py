1215acc0965f9d223e19f9558e5fea85af02c163
aberno2000 <aberndi08@gmail.com>
2026-04-16 13:22:44 +0300

    homework

---
+++ quiz_data.py
@@ -0,0 +1,54 @@
+quiz_data = [
+    {
+        'question': 'The most famous russian poet?',
+        'options': ['Fyodor Dostoyevsky', 'Aleksander Pushkin', 'Lev Tolstoy', 'Anton Chekhov'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Which city used to be the capital of Russia?',
+        'options': ['Volgograd', 'Saint Peterburg', '', 'Vladivastok', 'Leningrad'],
+        'correct_option': 1
+    },
+    {
+        'question': 'How many carbon atoms does propane has?',
+        'options': ['1', '2', '3', '4'],
+        'correct_option': 2
+    },
+    {
+        'question': 'Какой родственая слова слову \'подушка\'?',
+        'options': ['уха', 'ухо', 'духа', 'сухо'],
+        'correct_option': 2
+    },
+    {
+        'question': 'What is the integral of a 1 dimmensional infinite gaussian function?',
+        'options': ['π', '1/π', 'exp(π)', 'sqrt(π)'],
+        'correct_option': 3
+    },
+    {
+        'question': 'Какой правильный ответ?',
+        'options': ['правильный', 'неправильный', 'сомнительный', 'коректный'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Корабли лавировали, лавировали, лавировали да не вылавировали',
+        'options': ['лавировали?', 'не вылавировали?', 'уплыли?', 'утанули?'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Minimum amout of moves to solve a 4x4 rubik\'s cube',
+        'options': ['1', '11', '22', '7'],
+        'correct_option': 1
+    },
+    {
+        'question': 'what operation is not closed for intergers',
+        'options': ['+', '-', '*', '/'],
+        'correct_option': 3
+    },
+    {
+        'question': 'Which element is heavier',
+        'options': ['Americium', 'Tungsten', 'Polonium', 'Moscovium'],
+        'correct_option': 3
+    },
+]
+
+
